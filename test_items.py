557221d7b403c1e670a4065528a5cb59ea226af0
Aleksandr <sav8817@mail.ru>
2023-10-28 07:02:58 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_cart_button(browser):
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30)
     assert browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > button")
